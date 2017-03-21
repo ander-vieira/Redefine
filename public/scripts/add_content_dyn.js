f708dd0c053095fbedbbd9306b7f3cd6a634ac5e
Ander Vieira <ander.vieira741@gmail.com>
2017-03-21 16:45:03 +0100

    Un formulario dinamico para añadir contenido

---
+++ public/scripts/add_content_dyn.js
@@ -0,0 +1,11 @@
+function change_display() {
+  if(document.getElementById('radio1').checked) {
+    document.getElementById('input_texto').style.display='block';
+    document.getElementById('input_imagen').style.display='none';
+  }
+
+  else if(document.getElementById('radio2').checked) {
+    document.getElementById('input_texto').style.display='none';
+    document.getElementById('input_imagen').style.display='block';
+  }
+}
